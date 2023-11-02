d2bc20f5b13a71955984f95f2818e35851274b20
Panda2807 <kalabukhov2004@gmail.com>
2023-11-02 23:05:54 +0200

    Init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,48 +10,48 @@ export const Header = () => {
             <span className="header_logo">LOGO</span>
             <nav className="header_nav">
                 <ul className="header_nav_list">
-                    <li className="header_nav_item"><a href="#">Find a Property</a></li>
-                    <li className="header_nav_item"><a href="#">Share Stories</a></li>
-                    <li className="header_nav_item"><a href="#">Rental Guides</a></li>
-                    <li className="header_nav_item"><a href="#">Main</a></li>
-                    <li className="header_nav_item"><a href="#">Download Mobile App</a></li>
+                    <a href="/" className="header_nav_item">Find a Property</a>
+                    <a href="/" className="header_nav_item">Share Stories</a>
+                    <a href="/" className="header_nav_item">Rental Guides</a>
+                    <a href="/" className="header_nav_item">Main</a>
+                    <a href="/" className="header_nav_item">Download Mobile App</a>
                 </ul>
             </nav>
             <div className="header_button">
                 <button className="button0" onClick={() => setModalActive(true)}>Sign up</button>
-                <button className="button2" onClick={() => setModalActive1(true)}>Sign in</button>
+                <button className="button2" onClick={() => setModalActive1(true)}>Sing in</button>
             </div>
             <Modal active={modalActive} setActive={setModalActive}>
-                <h2>Registration</h2>
-                <form id="registration-form">
-                    <div className="form-group">
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" id="name" name="name" required />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" required />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name="password" required />
-                    </div>
-                    <div className="center"><button type="submit" className="button1">Sign up</button></div>
-                </form>
+                    <h2>Registration</h2>
+                    <form id="registration-form">
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" id="name" name="name" required />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="email" id="email" name="email" required />
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <input type="password" id="password" name="password" required />
+                        </div>
+                        <div className="center"><button type="submit" className="button1">Sign up</button></div>
+                    </form>
             </Modal>
             <Modal active={modalActive1} setActive={setModalActive1}>
-                <h2>Login</h2>
-                <form id="login-form">
-                    <div className="form-group">
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" id="name" name="name" required />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name="password" required />
-                    </div>
-                    <div className="center"><button type="submit" className="button1">Sign in</button></div>
-                </form>
+            <h2>Login</h2>
+                    <form id="registration-form">
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" id="name" name="name" required />
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <input type="password" id="password" name="password" required />
+                        </div>
+                        <div className="center"><button type="submit" className="button3">Sign in</button></div>
+                    </form>
             </Modal>
         </header>
     );
