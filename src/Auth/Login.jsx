4f877c0f6b662dae0ab479cd73c90ef3336f87bf
Panda2807 <kalabukhov2004@gmail.com>
2023-11-07 20:25:31 +0200

    Logtest1

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -9,30 +9,30 @@ export const Login = (props) => {
     
 
     const handleLoginSubmit = async (e) => {
-        e.preventDefault();
-        if (!name || !password) {
-          setError("Будь-ласка, заповніть усі поля");
-        } else {
+      e.preventDefault();
+      if (!name || !password) {
+          setError("Будь ласка, заповніть усі поля.");
+      } else {
           try {
-            setError("");
-            const response = await axios.post("http://ec2-16-171-234-234.eu-north-1.compute.amazonaws.com/api/signin", 
-            {
-              name,
-              password,
-            }
-          );
-            if (response.data.message === "LOGGED IN") {
-              // Викликайте функцію props.onFormSwitch, якщо потрібно переключити форму
-              // або виконайте іншу логіку залежно від вашого використання
-            } else if (response.data.error) {
-              setError(response.data.error);
-            } 
+              setError(""); // Очистіть попереднє повідомлення про помилку
+              const response = await axios.post("http://ec2-16-171-234-234.eu-north-1.compute.amazonaws.com/api/signin", 
+              {
+                  name,
+                  password,
+              });
+
+              if (response.data.message === "LOGGED IN") {
+                  // Викликайте функцію props.onFormSwitch, якщо потрібно переключити форму
+                  // або виконайте іншу логіку залежно від вашого використання
+              } else if (response.data.error) {
+                  setError(response.data.error); // Виведіть помилку з сервера
+              }
           } catch (error) {
-            console.log(error);
-            setError("Виникла помилка");
+              console.log(error);
+              setError("Виникла помилка на сервері."); // Виведіть власну помилку
           }
-        }
-    };
+      }
+  };
 
     return (
         <div className ="box">   
