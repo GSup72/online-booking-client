e83e9dc2db028454e5a99103579406c17f5bf48f
Panda2807 <kalabukhov2004@gmail.com>
2023-12-25 19:21:17 +0200

    second p

--- src/components/Link1.jsx
+++ src/components/Link1.jsx
@@ -1,6 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Header.css";
+import "./link.css";
+import Modal from "../Modal/Modal";
+
 
 export const Link1 = () => {
-    return (  <div><h1>123121233</h1></div>  );
+    const [modalActive, setModalActive] = useState(false);
+    return ( 
+        <div>
+        <header className="header">
+            <span className="header_logo">LOGO</span>
+            <nav className="header_nav">
+                <ul className="header_nav_list">
+                    <a href="/" className="header_nav_item">Find a Property</a>
+                    <a href="/" className="header_nav_item">Share Stories</a>
+                    <a href="/" className="header_nav_item">Rental Guides</a>
+                    <a href="/" className="header_nav_item">Main</a>
+                    <a href="/" className="header_nav_item">Download Mobile App</a>
+                </ul>
+            </nav>
+            <div className="header_button">
+                <button className="button0" onClick={() => setModalActive(true)}>Maps</button>
+            </div>
+            <Modal active={modalActive} setActive={setModalActive}>
+            <iframe title="Google Maps" width = "470px" height = "450px" src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2638.073250904742!2d22.264758976766736!3d48.60844111826336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473918f99ce99a35%3A0x5957f9d85e6d5b0f!2z0KLQvtC60ZbQvg!5e0!3m2!1suk!2sua!4v1703192471335!5m2!1suk!2sua"></iframe>
+            </Modal>
+        </header>
+
+    <div class="gallery-container">
+    <div class="main-image">
+      <img src="https://a0.muscache.com/im/pictures/147d730e-3d4e-4d63-bd43-02ad14354812.jpg?im_w=1200" alt="Головне зображення"/>
+    </div>
+    <div class="small-images">
+      <img src="https://a0.muscache.com/im/pictures/00869531-3007-4ba5-ae26-2d6c1bbfa7dd.jpg?im_w=720" alt="Маленьке зображення 1"/>
+      <img src="https://a0.muscache.com/im/pictures/0de6c6a8-f4b0-4940-bfbe-24ed045d75aa.jpg?im_w=720" alt="Маленьке зображення 2"/>
+      <img src="https://a0.muscache.com/im/pictures/936e521f-8a9a-4a36-8f40-64b221b04f1a.jpg?im_w=720" alt="Маленьке зображення 3"/>
+      <img src="https://a0.muscache.com/im/pictures/4a83360e-9f62-45ca-b084-5d59f6d1e47c.jpg?im_w=1200" alt="Маленьке зображення 4"/>
+    </div>
+  </div>
+
+
+  <div class="apartments">
+    <div class="left-section">
+      <h2>Помешкання для оренди цілком</h2>
+      <p>Місто Brno</p>
+      <div class="squares">
+      <div class="square">
+        <img src={require('../icon/icon1.png')} alt="Іконка 1"/>
+        <p>3 Спальні</p>
+      </div>
+      <div class="square">
+      <img src={require('../icon/icon2.png')} alt="Іконка 2"/>
+        <p>2 Ванні комнати</p>
+      </div>
+      <div class="square">
+      <img src={require('../icon/icon3.png')} alt="Іконка 3"/>
+        <p>2 Парковочних місця</p>
+      </div>
+      <div class="square">
+      <img src={require('../icon/icon4.png')} alt="Іконка 4"/>
+        <p>1 Домашню тварину дозволено</p>
+        </div>
+      </div>
+      <h2>Описання квартири</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi ipsa beatae cumque est. Quis enim nisi repudiandae debitis, optio quaerat quae vel, esse modi in impedit quod deserunt necessitatibus eligendi!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi ipsa beatae cumque est. Quis enim nisi repudiandae debitis, optio quaerat quae vel, esse modi in impedit quod deserunt necessitatibus eligendi!</p>
+    </div>
+    <div class="right-section">
+    <div className="anketa"> 
+      <h2>$ 1000 - $ 3000</h2>
+      <p>Коротки період: $ 1000</p>
+      <p>Середній період: $ 2000</p>
+      <p>Довгий період: $ 3000</p>
+      <button><b>Резервувати</b></button>
+      <p>Контакти</p>
+    </div>
+    </div>  
+  </div>
+
+  <footer class="footer">
+    <div class="footer-item">
+      <h2 className="log">LOGO</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur <br /> 
+      adipiscing elit, sed do eiusmod tempor incididunt <br /> 
+      ut labore et dolore magna aliqua.  </p>
+    </div>
+    <div class="footer-item">
+      <h2>COMPANY</h2>
+      <ul>
+        <li>About Us</li>
+        <li>Legal Information</li>
+        <li>Contact Us</li>
+        <li>Blogs</li>
+      </ul>
+    </div>
+    <div class="footer-item">
+      <h2>Help Center</h2>
+      <ul>
+        <li>Find a Property</li>
+        <li>How to Host</li>
+        <li>Why Us?</li>
+        <li>FAQs</li>
+        <li>Rental Guides</li>
+      </ul>
+    </div>
+    <div class="footer-item">
+      <h2>Contact Info</h2>
+      <ul>
+        <li>Phone: +1234567890</li>
+        <li>Email: company@gmail.com</li>
+        <li>Location: Tokio, Uzhorod</li>
+        <li>
+            
+            <button className="buttona" onClick={() => setModalActive(true)}>Maps</button>
+            <Modal active={modalActive} setActive={setModalActive}>
+            <iframe name = "myframe" title="Google Maps" width = "470px" height = "450px" src = "https://www.google.com/maps/embed/v1/place?q=Tokyo+Shopping+Center,+Uzhgorod&key=YOUR_API_KEY&center=Uzhgorod"></iframe>
+            </Modal>
+        </li>
+      </ul>
+    </div>
+  </footer>
+
+
+
+    </div>
+
+    );
 };
 
